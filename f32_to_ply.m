682436bee07a4c0a029346311bea5a1074e8b4df
Tetsuya Idota <tidota@hawaii.edu>
2018-02-02 11:00:23 -1000

    feature: removes redundant points withing a given rad

--- f32_to_ply.m
+++ f32_to_ply.m
@@ -7,27 +7,62 @@
 % The file must be placed under the directory "CAVES_code"
 %
 % Parameters:
-% - fname_f32: the file name of the f32 file
-% - fname_ply: the file name of the ply file
-% - downsize(optional): if given, the size of data is downsized to this number
+% - fname_f32:
+%     the file name of the f32 file
+% - fname_ply:
+%     the file name of the ply file
+% - radius_skip(optional):
+%     if given, eliminates points within this radius of another
+% - downsize(optional):
+%     if given, the size of data is downsized to this number
 %
-function f32_to_ply(fname_f32, fname_ply, downsize)
-
-pkg load statistics
+function f32_to_ply(fname_f32, fname_ply, radius_skip, downsize)
 
 printf('loading f32...\n');
 fflush(stdout);
 PC = read_pts_binary_float32(fname_f32);
 
 if exist('downsize','var')
-    cd 'point_cloud'
+    pkg load statistics % for octave
+    % to use the downsample_point_cloud function on octave,
+    % the statistics package is required.
+    
     printf('downsizing...\n');
     fflush(stdout);
+    cd 'point_cloud'
     PC = downsample_point_cloud(PC,downsize);
     cd ..
 end
 
 num = size(PC,1);
+
+if exist('radius_skip','var') & radius_skip > 0
+    printf('eliminating redundant points...');
+    fflush(stdout);
+
+    PC_new = PC(1,:);
+    for i = 2:num
+        num_new = size(PC_new,1);
+        f_insert = true;
+        for j = 1:num_new
+            if(norm(PC_new(j,:)-PC(i,:))<radius_skip)
+                f_insert = false;
+            end
+        end
+        if(f_insert)
+            PC_new = [PC_new; PC(i,:)];
+        end
+
+        if (mod(i,100) == 0)
+            printf('%d out of %d resampled\n',i,num);
+            fflush(stdout);
+        end
+    end
+
+    PC = PC_new;
+    num = size(PC,1);
+end
+
 printf('# of points: %d\n',num);
 
 fp = fopen(fname_ply,'w');
@@ -46,6 +81,7 @@ else
     fprintf(fp,'end_header\n');
     for i = 1:num
         fprintf(fp,'%f %f %f\n',PC(i,:));
+        
         if(mod(i,100) == 0)
             printf('%d out of %d points written\n',i,num);
             fflush(stdout);
